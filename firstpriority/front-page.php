9ddd6c32f01ad58a10a8c26cdc9f76c99ced1d27
taki <hideki302@ezweb.ne.jp>
2021-01-05 16:20:13 +0900

    firstpriorityのファイルをアップロードしました！

---
+++ firstpriority/front-page.php
@@ -0,0 +1,661 @@
+<?php get_header(); ?>
+
+
+
+<!-- メインセクション -->
+
+<div class="container">
+
+
+  <div class="pagemove" id="section-1" data-section-name="section1">
+    <h1 class="firename-h1" id="consept">株式会社ファーストプライオリティ</h1>
+    <div class="section-target">
+      <div class="target-name-set">
+        <div class="target-name-en">MISSION</div>
+        <div class="target-name-ja">存在意義​</div>
+      </div>
+      <div class="target-text-set">
+        <p class="target-text-big">患者と医療提供者の距離を縮め、医療へのアクセスをよりスムーズにする事で、<br>人々の健康と生活の質を向上させる。</p>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+
+<div class="pagemove" id="section-target-2" data-section-name="section2">
+  <div class="section-target section-target-sub">
+    <div class="target-name-set">
+      <div class="target-name-en">VISION</div>
+      <div class="target-name-ja">目指す​姿</div>
+    </div>
+    <div class="target-text-set">
+      <p class="target-text">医療におけるオンラインとオフラインを融合させるサービスを提供する事で、人々の医療へのアクセスをスムーズにする</p>
+      <p class="target-text">患者と医療提供者が対等にバディを組み、ともに健康に向き合えるようにする</p>
+    </div>
+  </div>
+</div>
+
+
+<div class="pagemove" id="section-target-3" data-section-name="section3">
+  <div class="section-target section-target-sub">
+    <div class="target-name-set">
+      <div class="target-name-en">VALUE</div>
+      <div class="target-name-ja">価値観・行動指針</div>
+    </div>
+    <div class="target-text-setextra target-text-set">
+      <p class="target-text">医療におけるDX（デジタルトランスフォーメーション）を推進させよう</p>
+      <p class="target-text">医療に関する無駄な時間を省き、人々が社会で活躍できる余裕を増やそう</p>
+      <p class="target-text">臨床現場に基づいた現実的・倫理観のある医療情報を整理・提供し、人々の医療リテラシーの向上に貢献しよう</p>
+      <p class="target-text">信頼できる医師や医療機関の情報を発信し、患者の情報不足や不安を解消しよう</p>
+      <p class="target-text">患者と医療提供者の橋渡しをしよう</p>
+      <p class="target-text">患者の目線を持ち、自分たちが受けたい医療サービスを提供しよう</p>
+      <p class="target-text">医療提供者の目線を持ち、医療のプロフェッショナルであり続けよう</p>
+    </div>
+  </div>
+</div>
+
+
+
+
+<!-- メインセクション -->
+
+
+
+
+<!-- お約束のセクション -->
+
+<div class="pagemove section-2-fullset" data-section-name="section4">
+
+  <div class="margin-point-1" id="promise"></div>
+
+  <div class="section-2-title">お約束</div>
+  <div class="section-2-title-text">ファーストプライオリティのご提供するバディドクターサービスです​</div>
+
+
+  <div class="section-2-picture-big-set">
+    <div class="section-2-picture-set">
+      <div class="image-contain"><img src="<?php echo get_template_directory_uri(); ?>/images/time.jpg" alt="時間の画像">
+      </div>
+      <div class="section-2-picture-text">医療にかける時間を短縮し、<br>無駄を省きます</div>
+    </div>
+    <div class="section-2-picture-set">
+      <div class="image-contain"><img src="<?php echo get_template_directory_uri(); ?>/images/heart.jpg" alt="青空ハート">
+      </div>
+      <div class="section-2-picture-text">医療リテラシーの向上に貢献し、<br>円滑な医療環境を提供します</div>
+    </div>
+    <div class="section-2-picture-set">
+      <div class="image-contain"> <img src="<?php echo get_template_directory_uri(); ?>/images/support.jpg"
+          alt="サポート画像"></div>
+      <div class="section-2-picture-text">医療機関を選ぶ際の情報不足や<br>不安を解消します</div>
+    </div>
+  </div>
+
+
+
+  <!-- お約束のセクションスマホ版 -->
+  <div class="promise-smartphone-set">
+
+  <div class="promise-smartphone-mini-1">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/時間経過のアイコン 1.png" alt="時間のアイコン">
+    <div class="promise-text">医療にかける時間を短縮し、無駄を省きます</div>
+  </div>
+
+
+  <div class="promise-smartphone-mini-2">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/かわいい医者のアイコン素材.png" alt="かわいい医者のアイコン素材">
+    <div class="promise-text">医療リテラシーの向上に貢献し、<br class="promise-indent">円滑な医療環境を提供します</div>
+  </div>
+
+  <div class="promise-smartphone-mini-3">
+  <img src="<?php echo get_template_directory_uri(); ?>/images/インターネットのアイコン3.png" alt="インターネットのアイコン3">
+    <div class="promise-text">医療機関を選ぶ際の情報不足や<br class="promise-indent">不安を解消します</div>
+  </div>
+
+
+  </div>
+
+
+
+
+
+  <!-- お約束のセクションスマホ版 -->
+
+
+</div>
+
+<!-- お約束のセクション -->
+
+
+
+
+
+
+
+<div class="promise-smartphone-border">
+
+<div class="executive-section-border"></div>
+
+</div>
+
+
+<div class="pagemove" data-section-name="section5">
+
+<div class="representative-set">
+  <div class="representative-picture"><img src="<?php echo get_template_directory_uri(); ?>/images/山本真徳.JPG" alt="代表取締役 山本真徳"></div>
+  <div class="representative-message-and-name">
+    <div class="representative-message">
+      <p>私は医師として25年以上に渡り、医療に携わってきました。日々の診療を重ねる中で、様々な不便と問題を感じてきました。特に私が大きな課題だと考えてきたことは、３つあります。</p>
+      <p>
+        １つ目は、医師と患者様が一緒になって健康に向かおうという姿勢が乏しいこと。本来であれば医師は自分や自分の家族がしてもらいたいと思う医療を患者様に提供すべきです。しかし、現状はそれができていない。背景には、医師の人間性やスキルの問題と、患者様の医療知識不足という２つの側面があります。この要因により、医療への抵抗感や恐怖が生じ、インフォームドコンセントが機能しないという事態が起きています。
+      </p>
+      <p>２つ目は、病院や薬局で長く時間がとられてしまうことです。患者の貴重な時間を無駄に使ってしまうことは、診療や治療が必要な方の足が遠のく原因となります。</p>
+      <p>
+        ３つ目は、ひとりの方の医療情報が一元管理されていないということです。そのため、病院を移るたびに、初診に必要な問診の手間がかかったり検査が重複したりしてしまいます。これは、重複投与に繋がる危険性もはらむ問題です。全ての医療機関で一人の方の医療情報にアクセスできるようにする必要があるのです。
+      </p>
+      <p>
+        こうした課題を改善していくことで、全ての人の健康と生活がより良いものになっていくはずです。そこで弊社は、『バディドクターフレーズ』、『バディドクターパス』、『バディドクター薬剤デリバリー』などのサービスで医療機関と患者様が共に健康増進を実現するプロジェクトを推進します。さらに、患者様側も医療リテラシーを高めていくことで、より医療従事者と対等に話をすることができます。そこで正しい医療情報を定期的に発信していくことも重視します。
+      </p>
+      <p>ファーストプライオリティは、患者様と医療従事者双方に新たな医療の枠組を提案します。</p>
+    </div>
+    <div class="fire-name">株式会社ファーストプライオリティ</div>
+    <div class="representative-name">代表取締役 山本真徳</div>
+  </div>
+
+</div>
+
+<!-- 代表メッセージのセクション -->
+
+
+<!-- 代表メッセージのスマホ版のセクション -->
+
+
+<div class="representative-smartphone-section" id="representative-smartphone-message">
+
+  <div class="representative-smartphone-picture">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/山本真徳.JPG"
+      alt="代表取締役 山本真徳"></div>
+
+  <div class="representative-smartphone-text">
+    <p>私は医師として25年以上に渡り、医療に携わってきました。日々の診療を重ねる中で、様々な不便と問題を感じてきました。特に私が大きな課題だと考えてきたことは、３つあります。</p>
+    <p>
+      １つ目は、医師と患者様が一緒になって健康に向かおうという姿勢が乏しいこと。本来であれば医師は自分や自分の家族がしてもらいたいと思う医療を患者様に提供すべきです。しかし、現状はそれができていない。背景には、医師の人間性やスキルの問題と、患者様の医療知識不足という２つの側面があります。この要因により、医療への抵抗感や恐怖が生じ、インフォームドコンセントが機能しないという事態が起きています。
+    </p>
+    <p>２つ目は、病院や薬局で長く時間がとられてしまうことです。患者の貴重な時間を無駄に使ってしまうことは、診療や治療が必要な方の足が遠のく原因となります。</p>
+    <p>
+      ３つ目は、ひとりの方の医療情報が一元管理されていないということです。そのため、病院を移るたびに、初診に必要な問診の手間がかかったり検査が重複したりしてしまいます。これは、重複投与に繋がる危険性もはらむ問題です。全ての医療機関で一人の方の医療情報にアクセスできるようにする必要があるのです。
+    </p>
+    <p>
+      こうした課題を改善していくことで、全ての人の健康と生活がより良いものになっていくはずです。そこで弊社は、『バディドクターフレーズ』、『バディドクターパス』、『バディドクター薬剤デリバリー』などのサービスで医療機関と患者様が共に健康増進を実現するプロジェクトを推進します。さらに、患者様側も医療リテラシーを高めていくことで、より医療従事者と対等に話をすることができます。そこで正しい医療情報を定期的に発信していくことも重視します。
+    </p>
+    ファーストプライオリティは、患者様と医療従事者双方に新たな医療の枠組を提案します。
+    <p class="smartphone-fire-name">株式会社ファーストプライオリティ</p>
+    <p class="smartphone-representative-name">代表取締役 山本真徳</p>
+  </div>
+
+</div>
+
+
+</div>
+
+
+
+<!-- 代表メッセージのスマホ版のセクション -->
+
+
+
+
+
+
+
+
+<div class="executive-section-border"></div>
+
+
+
+
+
+<div class="pagemove representative-profile" data-section-name="section6">
+  <p class="representative-profile-passage">代表取締役 山本真徳</p>
+
+  <p>宮崎県延岡市出身</p>
+  <div class="representative-profile-mini-set">
+    <div class="representative-profile-year">1993年</div>
+    <div class="representative-profile-more-mini-set">
+      <div class="representative-profile-year-text">順天堂大学医学部卒業の後に順天堂大学病院</div>
+      <div class="representative-profile-year-text">労働事業団東京労災病院などの基幹病院で臨床経験</div>
+    </div>
+  </div>
+  <div class="representative-profile-mini-set">
+    <div class="representative-profile-year">2006年</div>
+    <div class="representative-profile-more-mini-set">
+      <div class="representative-profile-year-text">医療法人社団順徳会理事長</div>
+    </div>
+  </div>
+  <div class="representative-profile-mini-set">
+    <div class="representative-profile-year">2018年</div>
+    <div class="representative-profile-more-mini-set">
+      <div class="representative-profile-year-text representative-profile-passage">
+        株式会社ファーストプライオリティ設立<br> 社会人ラグビートップリーグ発足当時より<br class="indent-smartphone">クボタスピアーズチームドクター歴任
+      </div>
+    </div>
+  </div>
+  <p>経歴</p>
+  <p>順天堂大学医学部 卒業</p>
+  <p>順天堂大学 整形外科 勤務</p>
+  <p>王子病院 整形外科 勤務</p>
+  <p>東京労災病院 整形外科 勤務</p>
+  <p>三郷順心病院 整形外科 勤務</p>
+  <p class="representative-profile-passage">順天堂浦安病院 整形外科 勤務</p>
+  <p>所属学会・認定医など</p>
+  <p>日本整形外科学会 整形外科専門医</p>
+  <p>日本整形外科学会 スポーツ整形外科認定医</p>
+  <p>日本整形外科学会 脊椎脊髄病認定医</p>
+  <p>運動器リハビリテーション 認定医</p>
+  <p>日本体育協会スポーツドクター 認定医</p>
+  <p>クボタスピアーズチームドクター</p>
+</div>
+
+
+
+<!-- 代表メッセージのセクション -->
+
+
+
+<div class="executive-section-border"></div>
+
+
+
+
+<!-- 役員プロフィールのセクション -->
+
+<div class="pagemove executive-set" data-section-name="section7">
+  <div class="executive-text">
+    <p class="executive-profile-passage">取締役 竹元和幸</p>
+    <p>宮崎県宮崎市出身</p>
+    <p>1986年 東京大学教養学部文化二類入学</p>
+    <p>1990年 株式会社リクルート入社、都内ソフトハウス転職</p>
+    <p>1993年 株式会社コルト・ヴォックス創業</p>
+    <p class="executive-profile-passage">現在、同社代表取締役社長</p>
+
+    <p class="executive-profile-passage">広範な業種・規模の企業を対象とした数多くのシステム開発・ネットワーク設計・ユーザサポートの現場を経験し、現在でもコードを書く現役のプログラマ。</p>
+
+    <p class="executive-profile-passage">豊富な現場経験を生かし、一般的な『システムコンサルティング』の枠に収まらない幅広い領域・形態でのコンサルタントとしても活動している。</p>
+
+    <p class="executive-profile-passage">現代医療の抱える様々な課題に対し、医師・患者に限らない関係者全員に役立つ解決策を現役の医師達と生み出していくことが目標。</p>
+
+    <p class="executive-profile-passage">
+      趣味は音楽活動。以前は吹奏楽・オーケストラでホルンを吹いていたが、ベーシストに転向して2017年からジャズ・フュージョンバンドに所属し、演奏活動を行っている。</p>
+
+    <p class="executive-profile-passage">日本酒をはじめとする世界の酒と、共に楽しむ世界の料理をこよなく愛する。つまりは大酒飲みの大喰らい。</p>
+  </div>
+  <div class="executive-picture"><img src="<?php echo get_template_directory_uri(); ?>/images/竹元和幸.JPG" alt="取締役 竹元 和幸">
+  </div>
+</div>
+
+
+
+
+
+
+<div class="executive-section-border"></div>
+
+
+
+
+
+
+
+
+
+
+<div class="pagemove executive-set" data-section-name="section8">
+  <div class="executive-text">
+    <p class="executive-profile-passage">取締役 山本哲郎</p>
+
+    <p>宮崎県延岡市出身</p>
+    <p>1990年 筑波大学医学専門学群入学</p>
+    <p>1996年 順天堂大学医学部附属病院外科研修医</p>
+    <p>1998年 順天堂大学第一外科入局</p>
+    <p>2016年 やまもと消化器内科・肛門科を開設</p>
+    <p class="executive-profile-passage">現在、同クリニック理事長</p>
+
+    <p class="executive-profile-passage">20年間消化器外科医として、主に消化器癌治療や救急医療などの過酷な医療現場で奮闘。地方から六本木まで様々な病院での勤務経験がある。</p>
+
+    <p class="executive-profile-passage">現在は2016年にオープンした自身のクリニックで、消化器疾患、肛門疾患を中心に地域医療に身を置く現役医師。</p>
+
+    <p class="executive-profile-passage">外科医時代から好きだった内視鏡検査は、現在も年間約2000件をこなす。</p>
+
+    <p class="executive-profile-passage">
+      無給無休の大学病院勤務や、開業医としての一時診療、医院経営など、多岐に渡る医師としての豊富な実体験を通して、現在は未来の医療現場をより良くするビジネスを生み出すことを目標としている。</p>
+
+    <p class="executive-profile-passage">猫と鯖の塩焼きが好き。</p>
+
+
+  </div>
+  <div class="executive-picture"><img src="<?php echo get_template_directory_uri(); ?>/images/山本哲朗.JPG" alt="取締役 山本哲郎">
+  </div>
+</div>
+
+
+<div class="executive-section-border"></div>
+
+
+<div class="pagemove executive-set" data-section-name="section9">
+  <div class="executive-text">
+
+    <p class="executive-profile-passage">取締役 小川明子</p>
+
+    <p>神奈川県鎌倉市出身</p>
+    <p>1988年 順天堂大学医学部入学</p>
+    <p>1993年 順天堂大学医学部附属病院外科研修医</p>
+    <p>1995年 順天堂大学第二外科入局</p>
+
+    <p class="executive-profile-passage">順天堂大学卒業後、消化器外科医として働いたのち、現在では、外科医、総合内科医、往診医、産業医として働いています。</p>
+
+    <p class="executive-profile-passage">患者さんと接しているうちに、多くの人が自分の体について知らなかったり、興味がなかったりすることにショックを受けました。</p>
+
+    <p class="executive-profile-passage">
+      『この状況を変えたい』と思い、病気の説明を詳しくし、いつどんな検査をしたか表にまとめ一緒に病気の治療に取り組んでいくようにしたところ、今では、私の患者さんは自分の採血データをきちんと把握でき、何がいけないのか、今後どうしたらいいのか理解できるようになりました。
+    </p>
+
+    <p>医療における私の目標は、</p>
+    <p>多くの人に自分の体や健康に対して少しでも興味を持ってもらうことです。</p>
+    <p>医療における正しい知識を得てもらうこと、</p>
+    <p>自ら予防できる病気は予防してもらうこと、</p>
+    <p class="executive-profile-passage">そして、少しでも健康寿命を伸ばし、健やかな老後を過ごしてもらうことを実現したいです。</p>
+    <p>そのために新しいこの会社で最善を尽くしたいと思っています。</p>
+
+
+  </div>
+  <div class="executive-picture"><img src="<?php echo get_template_directory_uri(); ?>/images/小川明子.jpg"
+      alt="取締役 小川明子"></div>
+</div>
+
+
+
+<div class="executive-section-border "></div>
+
+
+
+<div class="pagemove executive-set" data-section-name="section10">
+  <div class="executive-text">
+    <p class="executive-profile-passage">取締役 懸田 健史</p>
+
+    <p>東京都大田区出身</p>
+    <p>1996年 順天堂大医学部入学</p>
+    <p class="executive-profile-passage">2003年 順天堂大学整形外科学教室入局</p>
+
+    <p class="executive-profile-passage">大学病院や地方の基幹病院などの勤務を経て、現在は東京都や千葉県の病院やクリニックにて手術、外来診療を行っている。</p>
+
+    <p class="executive-profile-passage">小学校ではソフトボールクラブ、中学高校ではハンドボール部、大学ではラグビー部に所属し、学生時代はそれぞれの競技に没頭していた。</p>
+
+    <p>社会人になってから趣味としてゴルフやフィットネスをはじめボディメイクのコンテストに出場し『メンズフィジーク Over40部門』で優勝したこともある。
+    </p>
+    <p class="executive-profile-passage"></p>最近になってサーフィンを始め、その魅力に引き込まれている。</p>
+
+    <p>自身がスポーツをする中で様々な怪我をした経験もあり、また高校や大学、社会人ラグビーチーム、プロ格闘技選手などのサポートを経験することでスポーツを通じたヘルスケアの重要性を感じた。</p>
+    <p>スポーツ選手だけでなく、より多くの人に痛みなく自由に動ける身体でいられるような医療やサービスを提供したいと考えている。</p>
+  </div>
+  <div class="executive-picture"><img src="<?php echo get_template_directory_uri(); ?>/images/懸田健史.JPG" alt="取締役 懸田 健史">
+  </div>
+</div>
+
+
+
+<div class="executive-section-border"></div>
+
+
+
+
+<div class="pagemove executive-set" data-section-name="section11">
+  <div class="executive-text">
+
+    <p class="executive-profile-passage">取締役 長濱 崇</p>
+    <p>東京都武蔵野市出身</p>
+    <p>2004年 帝京大学法学部卒業</p>
+    <p class="executive-profile-passage">いかいクリニック、整骨院等を経て現在下総中山整形外科に勤務</p>
+
+    <p>所有資格</p>
+    <p>柔道整復師</p>
+    <p>NASA-PEZ</p>
+    <p>Certified Running Assessment Specialist</p>
+
+    <p>私は柔道整復師として、整骨院や整形外科で勤務した経歴があります。</p>
+    <p class="executive-profile-passage">その中で、現代の医療事情がデジタルシフトをできていないことを切に感じました。</p>
+
+    <p class="executive-profile-passage">
+      IT化やAI化が急速に進み医療技術も目覚ましいスピードで進歩していく中で、スキルが整ってもそれを発信するツールやコミュニティが少ないように見受けられます。１つの医療機関だけで情報を管理する時代は終わったのです。</p>
+
+    <p class="executive-profile-passage">
+      自分の身体は己で認知していくことが重要であり、常に自己管理は必要だど考えています。ジムに通い健康的な身体を維持することや、栄養面も留意した健康管理システムの構築、そうしたアイデンティティを確立することが今後の課題でしょう。
+    </p>
+    <p class="executive-profile-passage">これからは、ITと様々な医療業界やフィットネス等を結びつける橋渡しの存在となれるような会社を目指していきたいと思ってます。</p>
+
+    <p class="executive-profile-passage">趣味は、アイスホッケー・スノーボード・キャンプ等でアウトドアメインに日々アクティブに活動中。</p>
+
+
+  </div>
+  <div class="executive-picture"><img src="<?php echo get_template_directory_uri(); ?>/images/長濱崇.JPG" alt="取締役 長濱 崇">
+  </div>
+</div>
+
+
+
+<div class="executive-section-border"></div>
+
+
+
+
+<div class="pagemove executive-set" data-section-name="section12">
+  <div class="executive-text">
+    <p class="executive-profile-passage">取締役 伊藤 誠一</p>
+
+    <p>東京都葛飾区柴又出身</p>
+    <p>1994年 星薬科大学入学</p>
+    <p>2004年 新東京病院勤務</p>
+    <p>2014年 アイセイ薬局勤務</p>
+    <p class="executive-profile-passage">現在、アイセイ薬局勤務 下総中山店で勤務</p>
+
+    <p class="executive-profile-passage">薬剤師として、臨床に関わってきました。</p>
+
+    <p class="executive-profile-passage">
+      病院勤務時には、病棟での服薬指導を行い、今では当たり前となったTPN製剤と抗がん剤の混注業務の確立に注力しました。医師、看護師等との他業種連携も積極的に行ってきました。</p>
+
+    <p class="executive-profile-passage">調剤薬局では、1日300〜400人の患者様が来局する店舗で管理薬剤師をしています。毎日、様々な心療科の多くの医薬品を扱っています。</p>
+
+    <p class="executive-profile-passage">
+      今、医療業界は大きな変革期を迎えています。病院、クリニックと調剤薬局の患者情報の共有、診察、服薬指導のデジタル化など安全かつ利便性の高い医療が求められていると思います。</p>
+
+    <p class="executive-profile-passage">今までの自身の経験を生かして、社会に貢献していきたいです。</p>
+
+  </div>
+  <div class="executive-picture"><img src="<?php echo get_template_directory_uri(); ?>/images/伊藤誠一.JPG" alt="取締役 伊藤 誠一">
+  </div>
+</div>
+
+
+<div class="executive-section-border"></div>
+
+
+<!-- 役員プロフィールのセクション -->
+
+
+
+
+
+
+<!-- 会社情報のセクション -->
+
+<div class="pagemove fireinfo" id="fire-information" data-section-name="section13">
+  <p>ファーストプライオリティの会社情報です。</p>
+
+  <table>
+    <tr>
+      <th>社名</th>
+      <td> 株式会社ファーストプライオリティ</td>
+    </tr>
+    <tr>
+      <th>設立​</th>
+      <td>2018年12月3日</td>
+    </tr>
+    <tr>
+      <th>本社</th>
+      <td>東京都品川区西五反田8-4-15 グリンデル広小路9F</td>
+    </tr>
+    <tr>
+      <th>資本金</th>
+      <td>3900万​</td>
+    </tr>
+  </table>
+
+</div>
+
+
+
+<!-- 会社情報のセクション -->
+
+
+
+
+</div>
+<!-- containerのクラス -->
+
+
+<div class="page-down-button" id="section-scroller-button"><span class="pagemovebuttton"><i class="caret"></i></span></div>
+
+
+
+
+
+
+<!-- お問い合わせはプラグイン使用します -->
+
+
+
+</div>
+
+
+
+
+
+
+
+
+<script>
+
+  // 背景画像自動切り替え
+
+  var images = ['url(<?php echo get_template_directory_uri(); ?>/images/main.png)', 'url(<?php echo get_template_directory_uri(); ?>/images/main-2.jpg)'];  // ランダム表示させたい画像のパス
+
+  $(function () {
+    var backgroundRandom = function () {
+      var number = Math.floor(Math.random() * images.length); // 0~3の数値を算出 
+      var selectedImg = images[number]; // 算出された数値を元に、1行目の配列から取り出す
+      $('#section-1,#section-target-2,#section-target-3').css('background-image', selectedImg); // cssにランダムに選ばれた画像を背景設定する
+    };
+    setInterval(backgroundRandom, 10000);  // 4000msごとにランダム切り替えを繰り返す
+  });
+
+
+
+  // 背景画像自動切り替え
+
+</script>
+
+
+
+<script>
+
+
+  // お約束のところの画像切り替え
+
+  $(document).ready(function () {
+    $('.slider').bxSlider({
+      auto: true,
+      pause: 5000,
+    });
+  });
+
+
+  // お約束のところの画像切り替え
+
+</script>
+
+
+
+
+
+
+
+
+
+<!-- ページダウンボタン -->
+
+<script>
+
+
+$(document).ready(function () {
+    $(".pagemove").sectionScroller({
+        // Options
+        scrollerButton: "#section-scroller-button"
+    });
+});
+
+
+
+</script>
+
+<!-- ページダウンボタン -->
+
+
+
+
+<!-- スクロールjs -->
+
+<script>
+
+var option = {
+  section : '.pagemove', // 対象を指定
+  easing: "swing", // イージングをしてい(jQueryのanimation)
+  scrollSpeed: 1200, // スクロール時の速度
+  scrollbars: true, // スクロールバーを表示するか
+};
+
+$(function () {
+  $.scrollify({
+    section: ".pagemove",
+  });
+
+  $(".js-scrollify-move").each(function () {
+    $(this).click(function () {
+      let href = $(this).attr("href");
+      $.scrollify.move(href);
+    });
+  });
+});
+
+</script>
+
+<!-- スクロールjs -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
